2e78d59c2f3d2c76db8f1315499829651f436f69
fi3 <fmerli1@gmail.com>
2017-04-07 16:52:10 +0200

    Model (state/store) structure and Model initializer

---
+++ src/Main.js
@@ -0,0 +1,28 @@
+// @flow
+
+
+import type {InOut} from './Models.js'
+import {Wallet, Transaction, Model} from './Models.js'
+
+
+// Initialize the Model (redux Store)
+
+
+const initialWallet =
+	  { privKey : ''
+		, pubKey : ''
+		}
+
+
+const inititialTransaction =
+	{ sign : 'In'
+	, amount : 22
+	, counterparty : 'foo'
+	}
+
+
+export const initialModel : Model =
+  Model.fromJS(
+		{ wallet : initialWallet
+		, transactions : [inititialTransaction]
+    });
--- src/Models.js
+++ src/Models.js
@@ -5,10 +5,30 @@ import * as Immutable from 'immutable';
 import ImmutableModel from 'flow-immutable-models';
 
 
+export type InOut
+  = "In"
+  | "Out"
+
+
 export type WalletModelType = 
-	{ address : string
-	, privKey : string
+	//
+	// pubKey is the address of the wallet like an IBAN
+	//
+	{ privKey : string
 	, pubKey : string
+	};
+
+
+export type TransactionModelType =
+  { sign : InOut
+  , amount : number
+  , counterparty : string
+  }
+
+
+export type ModelModelType =
+  { wallet : WalletModelType
+	, transactions : Array<TransactionModelType>
 	};
 
 // /////////////////////////////////////////////////////////////////////////////
@@ -20,40 +40,118 @@ export type WalletModelType =
 //
 // /////////////////////////////////////////////////////////////////////////////
 export class Wallet extends ImmutableModel {
-    static fromJS(json: WalletModelType): Wallet {
-        const state: Object = Object.assign({}, json);
-        return new this(Immutable.Map(state));
-    }
+  static fromJS(json: WalletModelType): Wallet {
+    const state: Object = Object.assign({}, json);
+    return new this(Immutable.Map(state));
+  }
 
-    toJS(): WalletModelType {
-        return {
-            address: this.address,
-            privKey: this.privKey,
-            pubKey: this.pubKey,
-        };
-    }
+  toJS(): WalletModelType {
+    return {
+      privKey: this.privKey,
+      pubKey: this.pubKey,
+    };
+  }
 
-    get address(): string {
-        return this._state.get('address');
-    }
+  get privKey(): string {
+    return this._state.get('privKey');
+  }
 
-    setAddress(address: string): this {
-        return this.clone(this._state.set('address', address));
-    }
+  setPrivKey(privKey: string): this {
+    return this.clone(this._state.set('privKey', privKey));
+  }
 
-    get privKey(): string {
-        return this._state.get('privKey');
-    }
+  get pubKey(): string {
+    return this._state.get('pubKey');
+  }
 
-    setPrivKey(privKey: string): this {
-        return this.clone(this._state.set('privKey', privKey));
-    }
+  setPubKey(pubKey: string): this {
+    return this.clone(this._state.set('pubKey', pubKey));
+  }
+}
 
-    get pubKey(): string {
-        return this._state.get('pubKey');
-    }
+// /////////////////////////////////////////////////////////////////////////////
+//
+// NOTE: THIS CLASS IS GENERATED. DO NOT MAKE CHANGES HERE.
+//
+// If you need to update this class, update the corresponding flow type above
+// and re-run the flow-immutable-models codemod
+//
+// /////////////////////////////////////////////////////////////////////////////
+export class Transaction extends ImmutableModel {
+  static fromJS(json: TransactionModelType): Transaction {
+    const state: Object = Object.assign({}, json);
+    return new this(Immutable.Map(state));
+  }
 
-    setPubKey(pubKey: string): this {
-        return this.clone(this._state.set('pubKey', pubKey));
-    }
+  toJS(): TransactionModelType {
+    return {
+      sign: this.sign,
+      amount: this.amount,
+      counterparty: this.counterparty,
+    };
+  }
+
+  get sign(): InOut {
+    return this._state.get('sign');
+  }
+
+  setSign(sign: InOut): this {
+    return this.clone(this._state.set('sign', sign));
+  }
+
+  get amount(): number {
+    return this._state.get('amount');
+  }
+
+  setAmount(amount: number): this {
+    return this.clone(this._state.set('amount', amount));
+  }
+
+  get counterparty(): string {
+    return this._state.get('counterparty');
+  }
+
+  setCounterparty(counterparty: string): this {
+    return this.clone(this._state.set('counterparty', counterparty));
+  }
+}
+
+// /////////////////////////////////////////////////////////////////////////////
+//
+// NOTE: THIS CLASS IS GENERATED. DO NOT MAKE CHANGES HERE.
+//
+// If you need to update this class, update the corresponding flow type above
+// and re-run the flow-immutable-models codemod
+//
+// /////////////////////////////////////////////////////////////////////////////
+export class Model extends ImmutableModel {
+  static fromJS(json: ModelModelType): Model {
+    const state: Object = Object.assign({}, json);
+    state.wallet = Wallet.fromJS(state.wallet);
+    state.transactions = Immutable.List(state.transactions).map(item => Transaction.fromJS(item));
+    return new this(Immutable.Map(state));
+  }
+
+  toJS(): ModelModelType {
+    return {
+      wallet: this.wallet.toJS(),
+      transactions: this.transactions.toArray().map(item => item.toJS()),
+    };
+  }
+
+  get wallet(): Wallet {
+    return this._state.get('wallet');
+  }
+
+  setWallet(wallet: Wallet): this {
+    return this.clone(this._state.set('wallet', wallet));
+  }
+
+  get transactions(): Immutable.List<Transaction> {
+    return this._state.get('transactions');
+  }
+
+  setTransactions(transactions: Immutable.List<Transaction>): this {
+    return this.clone(this._state.set('transactions', transactions));
+  }
 }
