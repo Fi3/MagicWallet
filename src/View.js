0e3bd16e30a13344620176d77bd08543589f9623
fi3 <fmerli1@gmail.com>
2017-04-07 22:05:15 +0200

    FIX tab/space in View.js (nvim do not know jsx:()

--- src/View.js
+++ src/View.js
@@ -8,22 +8,22 @@ import {Wallet, Transaction, Model} from './Models.js'
 
 function RenderAddress(props : {wallet: Wallet}) {
   return <h1>{props.wallet.pubKey}</h1>;
-  }
+}
 
 
 function RenderTransaction(props : {transaction: Transaction}) {
   return <p>{props.transaction.amount}</p>;
-	}
+}
 
 
 export function View(model : Model) {
-	return (
-		<div>
-      <div>
-        <RenderAddress wallet={model.wallet} />
-      </div>
-        <RenderTransaction transaction={model.transactions.last()} />
-		  <div>
-		  </div>
-		</div>
-	);}
+  return (
+  <div>
+    <div>
+      <RenderAddress wallet={model.wallet} />
+    </div>
+      <RenderTransaction transaction={model.transactions.last()} />
+    <div>
+    </div>
+  </div>
+);}
