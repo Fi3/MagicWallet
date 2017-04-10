f05183988dda91c746bda9c5337786d4d7acfe1b
fi3 <fmerli1@gmail.com>
2017-04-10 21:59:23 +0200

    the initial state should contain 0 tsx

--- src/Main.js
+++ src/Main.js
@@ -19,17 +19,10 @@ const initialWallet =
   }
 
 
-const inititialTransaction =
-  { sign : 'In'
-  , amount : 22
-  , counterparty : 'foo'
-  }
-
-
 export const initialModel : Model =
   Model.fromJS(
   { wallet : initialWallet
-  , transactions : [inititialTransaction]
+  , transactions : []
   , payForm :
     { address : ''
     , amount : ''
